51525b844acd4b9188dfd656d132dc9726f8239b
AlRyumin <alryumin@gmail.com>
2020-07-23 06:35:22 +0300

    Constant reverted.

---
+++ app/src/main/java/com/example/popularmoviesapp/data/Constant.java
@@ -0,0 +1,13 @@
+package com.example.popularmoviesapp.data;
+
+public final class Constant {
+    public static final String API_KEY = ""; // themoviedb API key
+
+    public static final int SORT_TYPE_POPULAR = 1;
+    public static final int SORT_TYPE_TOP_RATED = 2;
+    public static final int SORT_TYPE_FAVORITE_MOVIES = 3;
+
+    public static final int IS_MOVIE_FAVORITE = 2352;
+    public static final String MOVIE_FAVORITE_KEY = "movie_favorite_key";
+    public static final String MOVIE_FAVORITE_ACTION = "movie_favorite_action";
+}
